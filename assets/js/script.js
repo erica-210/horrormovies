7580b2df3c1bcd171343777e78f44050a3957d3c
erica-210 <137020409+erica-210@users.noreply.github.com>
2023-10-03 19:05:28 -0500

    working on api fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,10 @@
 let startButton = document.querySelector('#startButton');
+var omdbApiKey = "45d7bdd6";
+var tmdbApiKey = "96e06e8f584c29c1ea0c1fe465d02637";
+var letsBegin;
+
 
 startButton.addEventListener('click', function(e) {
+    event.preventDefault();
+    letsBegin = startButton.value;
 });

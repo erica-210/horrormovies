72c942f293b4b57cc033436d2e7808deb815d000
erica-210 <137020409+erica-210@users.noreply.github.com>
2023-10-03 19:26:22 -0500

    omdb api fetched

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,33 @@ var omdbApiKey = "45d7bdd6";
 var tmdbApiKey = "96e06e8f584c29c1ea0c1fe465d02637";
 var letsBegin;
 
+function getApi(omdbURL) {
+    fetch(omdbURL, {
+        method: 'GET'
+       })
+       
+    .then(function (response) {
+       if (response.ok) {
+           console.log(response);
+       } else {
+           console.log(status);
+       }
+       return response.json();
+    })
+
+    .then(function(data) {
+        console.log(data);
+    })
+
+    .catch(function (error) {
+        alert('ERROR');
+      });
+}
 
 startButton.addEventListener('click', function(e) {
     event.preventDefault();
     letsBegin = startButton.value;
+    var omdbURL = "http://www.omdbapi.com/?i=tt3896198&apikey=45d7bdd6"
+    getApi(omdbURL);
+    console.log("The clicker, clicks");
 });

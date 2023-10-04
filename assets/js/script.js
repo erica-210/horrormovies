015f207db9c3f2f188e2e4a9bf2b17f814545e3c
erica-210 <137020409+erica-210@users.noreply.github.com>
2023-10-03 19:33:29 -0500

    created fetch function for tmdb api

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,10 +26,35 @@ function getApi(omdbURL) {
       });
 }
 
+function getApi(tmdbURL) {
+    fetch(tmdbURL, {
+        method: 'GET'
+       })
+       
+    .then(function (response) {
+       if (response.ok) {
+           console.log(response);
+       } else {
+           console.log(status);
+       }
+       return response.json();
+    })
+
+    .then(function(data) {
+        console.log(data);
+    })
+
+    .catch(function (error) {
+        alert('ERROR');
+      });
+}
+
 startButton.addEventListener('click', function(e) {
     event.preventDefault();
     letsBegin = startButton.value;
-    var omdbURL = "http://www.omdbapi.com/?i=tt3896198&apikey=45d7bdd6"
+    var omdbURL = "http://www.omdbapi.com/?i=tt3896198&apikey=45d7bdd6";
+    var tmbdURL = "https://api.themoviedb.org/3/movie/550?api_key=96e06e8f584c29c1ea0c1fe465d02637";
     getApi(omdbURL);
+    getApi(tmbdURL);
     console.log("The clicker, clicks");
 });

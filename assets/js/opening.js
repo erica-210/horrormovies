a62bd405073cbe054326f4375f56216f4b2c98e2
erica-210 <137020409+erica-210@users.noreply.github.com>
2023-10-05 20:04:22 -0500

    set links to pages and got start button to link

--- assets/js/opening.js
+++ assets/js/opening.js
@@ -1,13 +1,14 @@
 let startButton = document.querySelector("#start");
+var link = document.querySelector("#favorites-link");
+
+//link.addEventListener("click", function(e) {
+   // window.location.href = "reccomended.html";
+    
+//});
 
 startButton.addEventListener("click", function (e) {
     event.preventDefault();
-    letsBegin = startButton.value;
-    var omdbURL = "https://www.omdbapi.com/?i=tt3896198&apikey=45d7bdd6";
-    var tmbdURL =
-      "https://api.themoviedb.org/3/movie/550?api_key=96e06e8f584c29c1ea0c1fe465d02637";
-    getApi(omdbURL);
-    getApi(tmbdURL);
-    
+    window.location.href = "reccomended.html";
     console.log("The clicker, clicks");
   });
+
